217149f541e504edf047a09c5fcea539d595ae81
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-07-31 23:21:25 -0700

    initialize the spring boot

---
+++ Project-Management-Board-API-Specification/src/main/java/com/example/Project/Management/Board/API/Specification/ProjectManagementBoardApiSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project.Management.Board.API.Specification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementBoardApiSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementBoardApiSpecificationApplication.class, args);
+	}
+
+}
---
+++ Project-Management-Board-API-Specification/src/test/java/com/example/Project/Management/Board/API/Specification/ProjectManagementBoardApiSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project.Management.Board.API.Specification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementBoardApiSpecificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
