b48e0580d5cfedad7b4da02d1398d9ddd1d9af12
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-08-05 08:10:48 -0700

    handle fetch all cards and create card

--- Project-Management-Board-API-Specification/src/main/resources/static/script.js
+++ Project-Management-Board-API-Specification/src/main/resources/static/script.js
@@ -1,39 +1,92 @@
-// Function to set the title of the board
-function setTitle() {
-    const titleInput = document.getElementById('titleInput');
-    const title = titleInput.value.trim(); // Trim any leading/trailing white spaces
-    const displayElement = document.getElementById('displayTitle');
+async function displayCardsBySection() {
+  const boardId = 2; // Assuming the card should be added to the board with ID 2
+  try {
+    // Fetch the list of cards for the board using the provided fetch code
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
 
-    // Check if the title is not empty
-    if (title !== '') {
-        displayElement.textContent = title;
-    } else {
-        displayElement.textContent = "Project Management Board"; // If the title is empty, show the default value
+    const response = await fetch("http://localhost:8080/api/boards/" + boardId + "/cards", requestOptions);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch cards. Status: ${response.status} ${response.statusText}`);
+    }
+    // Parse the response body as JSON
+    const data = await response.json();
+    console.log("cards:", data); // Add this line to inspect the value of `cards`
+    const cards = data.cards; // Extract the cards array from the response
+
+    // Get the containers for each section
+    const todoContainer = document.getElementById('todo');
+    const inProgressContainer = document.getElementById('inprogress');
+    const doneContainer = document.getElementById('done');
+
+    // Clear existing content in the containers
+    todoContainer.innerHTML = '<h2>To Do</h2>';
+    inProgressContainer.innerHTML = '<h2>In Progress</h2>';
+    doneContainer.innerHTML = '<h2>Done</h2>';
+
+    // Loop through the cards and create HTML elements to display them
+    cards.forEach((card, index) => {
+      const cardElement = document.createElement('div');
+      cardElement.classList.add('card'); // Add the 'card' class for styling
+      cardElement.innerHTML = `
+        <h3>Card ID: ${index + 1}</h3>
+        <p>Title: ${card.title}</p>
+        <p>Description: ${card.description}</p>
+      `;
+      // Add the card to the corresponding container based on the section
+      if (card.section === '1' | card.section === "TODO") {
+        todoContainer.appendChild(cardElement);
+      } else if (card.section === '2' || card.section === "In Progress") {
+        inProgressContainer.appendChild(cardElement);
+      } else if (card.section === '3'  || card.section === "Done") {
+        doneContainer.appendChild(cardElement);
+      }
+    });
+  } catch (error) {
+    console.error('Error fetching cards:', error);
+    alert('An error occurred while fetching cards.');
+  }
+}
+async function createNewCard() {
+  const boardId = 2; // Assuming the card should be added to the board with ID 2
+  const titleInput = document.getElementById('createCardTitleInput');
+  const descInput = document.getElementById('createCardDescInput');
+  const sectionSelect = document.getElementById('createCategorySelect');
+
+  // Get the values from the input fields
+  const cardData = {
+    title: titleInput.value,
+    description: descInput.value,
+    section: sectionSelect.value,
+  };
+
+  try {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cardData),
+      redirect: 'follow',
+    };
+
+    const response = await fetch(`http://localhost:8080/api/boards/${boardId}/cards`, requestOptions);
+    if (!response.ok) {
+      throw new Error(`Failed to create card. Status: ${response.status} ${response.statusText}`);
     }
 
-    titleInput.value = ''; // Clear the input box after processing
+    const createdCard = await response.json();
+    console.log('Created Card:', createdCard);
+    alert('New card created successfully!');
+    // You can now call the displayCardsBySection() function to refresh the displayed cards.
+    displayCardsBySection();
+  } catch (error) {
+    console.error('Error creating card:', error);
+    alert('An error occurred while creating the card.');
+  }
 }
 
-// Function to populate the "Select ID" dropdowns
-function populateUpdateDropdown() {
-    // Get the table and dropdown elements
-    const table = document.querySelector(".project-table");
-    const selectIDDropdown = document.getElementById("anotherSelectID");
-
-    // Clear existing options in the dropdown
-    selectIDDropdown.innerHTML = '<option selected disabled>Select ID From Here</option>';
-
-    // Loop through all cards and add their IDs to the dropdown
-    const rowCount = table.rows.length;
-    for (let i = 1; i < rowCount; i++) {
-        const cardID = table.rows[i].querySelector("b").innerText;
-        const option = document.createElement("option");
-        option.text = cardID;
-        selectIDDropdown.add(option);
-    }
-}
-
-// Function to execute on page load
-document.addEventListener('DOMContentLoaded', () => {
-    populateUpdateDropdown();
-});
+    // Call the function to fetch and display cards when the page loads
+    displayCardsBySection();
