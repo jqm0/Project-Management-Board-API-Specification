d1db5db2dfea6a7769a278e31f5637d862caba0e
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-08-06 00:55:47 -0700

    Handle Delete fuction and dropdown

--- Project-Management-Board-API-Specification/src/main/resources/static/script.js
+++ Project-Management-Board-API-Specification/src/main/resources/static/script.js
@@ -24,14 +24,14 @@ async function displayCardsBySection() {
     const doneContainer = document.getElementById('done');
 
     // Get the select element for the card IDs
-    const selectElement = document.getElementById('anotherSelectID2');
-
+    const selectElement = document.getElementById('anotherSelectID');
+    const selectElement2 = document.getElementById('anotherSelectID2');
     // Clear existing content in the containers and the select element
     todoContainer.innerHTML = '<h2>To Do</h2>';
     inProgressContainer.innerHTML = '<h2>In Progress</h2>';
     doneContainer.innerHTML = '<h2>Done</h2>';
     selectElement.innerHTML = '<option selected disabled>Select ID From Here</option>';
-
+    selectElement2.innerHTML = '<option selected disabled>Select ID From Here</option>';
     // Loop through the cards and create HTML elements to display them
     cards.forEach((card, index) => {
       const cardElement = document.createElement('div');
@@ -40,6 +40,7 @@ async function displayCardsBySection() {
         <h3>Card ID: ${index + 1}</h3>
         <p>Title: ${card.title}</p>
         <p>Description: ${card.description}</p>
+
       `;
 
       // Add the card to the corresponding container based on the section
@@ -52,10 +53,15 @@ async function displayCardsBySection() {
        }
 
       // Populate the select element with card IDs
-      const optionElement = document.createElement('option');
-      optionElement.value = card.id; // Assuming 'id' is the property that holds the card ID
-      optionElement.textContent = card.id;
-      selectElement.appendChild(optionElement);
+      const option2 = document.createElement('option');
+      option2.value = card.id;
+      option2.text = `Card ID: ${card.id}`;
+      anotherSelectID.appendChild(option2);
+
+      const option3 = document.createElement('option');
+      option3.value = card.id;
+      option3.text = `Card ID: ${card.id}`;
+      anotherSelectID2.appendChild(option3);
     });
   } catch (error) {
     console.error('Error fetching cards:', error);
@@ -101,6 +107,44 @@ async function createNewCard() {
     alert('An error occurred while creating the card.');
   }
 }
+async function handleDelete() {
+  const cardIdDropdown = document.getElementById('cardIdDropdown');
+  const selectedCardId = anotherSelectID2.value;
+
+  if (selectedCardId) {
+    await deleteCard(selectedCardId);
+    // After deleting the card, you may want to update the UI to remove the deleted card from the display
+    // You can do this by calling the displayCardsBySection function again to refresh the cards on the page.
+    displayCardsBySection();
+  } else {
+    alert('Please select a card ID to delete.');
+  }
+}
+async function deleteCard(cardId) {
+  const boardId = 2; // Assuming the card should be deleted from the board with ID 2
+  try {
+    const requestOptions = {
+      method: 'DELETE',
+      redirect: 'follow'
+    };
+
+    const response = await fetch(`http://localhost:8080/api/boards/${boardId}/cards/${cardId}`, requestOptions);
+    if (!response.ok) {
+      throw new Error(`Failed to delete card. Status: ${response.status} ${response.statusText}`);
+    }
+
+    const result = await response.text();
+    console.log(result); // Log the response text
+
+    // After successfully deleting the card, you may want to update the UI to remove the deleted card from the display
+    // You can do this by removing the card element from the corresponding container based on its ID
+
+  } catch (error) {
+    console.error('Error deleting card:', error);
+    alert('An error occurred while deleting the card.');
+  }
+}
+
 
     // Call the function to fetch and display cards when the page loads
     displayCardsBySection();
